009f98626ae6e8d5ef5b3cb7f4cc0b0748451ce4
Viktor Poluksht <vpoluksht@gmail.com>
2024-09-16 11:54:23 +0100

    feat: added support for SharedReplacingMergeTree
    Signed-off-by: Viktor Poluksht <vpoluksht@gmail.com>

--- sink-connector/src/main/java/com/altinity/clickhouse/sink/connector/db/DBMetadata.java
+++ sink-connector/src/main/java/com/altinity/clickhouse/sink/connector/db/DBMetadata.java
@@ -1,6 +1,5 @@
 package com.altinity.clickhouse.sink.connector.db;
 
-import static com.altinity.clickhouse.sink.connector.db.ClickHouseDbConstants.CHECK_DB_EXISTS_SQL;
 import com.clickhouse.jdbc.ClickHouseConnection;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.MutablePair;
@@ -16,6 +15,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
+import static com.altinity.clickhouse.sink.connector.db.ClickHouseDbConstants.CHECK_DB_EXISTS_SQL;
+
 public class DBMetadata {
 
     private static final Logger log = LogManager.getLogger(DBMetadata.class);
@@ -26,7 +27,7 @@ public class DBMetadata {
         REPLACING_MERGE_TREE("ReplacingMergeTree"),
 
         REPLICATED_REPLACING_MERGE_TREE("ReplicatedReplacingMergeTree"),
-
+        SHARED_REPLACING_MERGE_TREE("SharedReplacingMergeTree"),
         MERGE_TREE("MergeTree"),
 
         DEFAULT("default");
@@ -44,6 +45,7 @@ public class DBMetadata {
 
     /**
      * Wrapper function to get table engine.
+     *
      * @param conn
      * @param tableName
      * @return
@@ -53,7 +55,7 @@ public class DBMetadata {
         MutablePair<TABLE_ENGINE, String> result;
         result = getTableEngineUsingSystemTables(conn, databaseName, tableName);
 
-        if(result.left == null) {
+        if (result.left == null) {
             result = getTableEngineUsingShowTable(conn, databaseName, tableName);
         }
 
@@ -62,6 +64,7 @@ public class DBMetadata {
 
     /**
      * Function to check if database exists by querying the information schema tables.
+     *
      * @param conn
      * @param databaseName
      * @return
@@ -74,13 +77,13 @@ public class DBMetadata {
             ResultSet rs = stmt.executeQuery(showSchemaQuery);
             if (rs != null && rs.next()) {
                 String response = rs.getString(1);
-                if(response.equalsIgnoreCase(databaseName)) {
+                if (response.equalsIgnoreCase(databaseName)) {
                     result = true;
                 }
             }
 
             rs.close();
-        } catch(SQLException se) {
+        } catch (SQLException se) {
             log.error("checkIfDatabaseExists exception", se);
             // ToDO: For some reason, this query throws SQLException when DB is not available.
 
@@ -94,6 +97,7 @@ public class DBMetadata {
      * Function to return Engine type for table.
      * This function calls the "create table" SQL
      * to get the schema of the table.
+     *
      * @param tableName
      * @return
      */
@@ -106,20 +110,20 @@ public class DBMetadata {
                 log.error("Error with DB connection");
                 return new MutablePair<>(null, null);
             }
-            try(Statement stmt = conn.createStatement()) {
+            try (Statement stmt = conn.createStatement()) {
                 String showSchemaQuery = String.format("show create table %s.`%s`", databaseName, tableName);
                 ResultSet rs = stmt.executeQuery(showSchemaQuery);
-                if(rs != null && rs.next()) {
-                    String response =  rs.getString(1);
-                    if(response.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.engine)) {
+                if (rs != null && rs.next()) {
+                    String response = rs.getString(1);
+                    if (response.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.engine)) {
                         result.left = TABLE_ENGINE.COLLAPSING_MERGE_TREE;
                         result.right = getSignColumnForCollapsingMergeTree(response);
-                    } else if(response.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.engine)) {
+                    } else if (response.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.engine)) {
                         result.left = TABLE_ENGINE.REPLACING_MERGE_TREE;
                         result.right = getVersionColumnForReplacingMergeTree(response);
-                    } else if(response.contains(TABLE_ENGINE.MERGE_TREE.engine)) {
+                    } else if (response.contains(TABLE_ENGINE.MERGE_TREE.engine)) {
                         result.left = TABLE_ENGINE.MERGE_TREE;
-                    }else {
+                    } else {
                         result.left = TABLE_ENGINE.DEFAULT;
                     }
                 }
@@ -127,7 +131,7 @@ public class DBMetadata {
                 stmt.close();
                 log.info("getTableEngineUsingShowTable ResultSet" + rs);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             log.info("getTableEngineUsingShowTable exception", e);
         }
 
@@ -139,8 +143,11 @@ public class DBMetadata {
 
     public static final String REPLACING_MERGE_TREE_VERSION_WITH_IS_DELETED = "23.2";
     public static final String REPLICATED_REPLACING_MERGE_TREE_VER_PREFIX = "ReplicatedReplacingMergeTree(";
+    public static final String SHARED_REPLACING_MERGE_TREE_VER_PREFIX = "SharedReplacingMergeTree(";
+
     /**
      * Function to extract the sign column for CollapsingMergeTree
+     *
      * @param createDML
      * @return Sign column
      */
@@ -148,7 +155,7 @@ public class DBMetadata {
 
         String signColumn = "sign";
 
-        if(createDML.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.getEngine())) {
+        if (createDML.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.getEngine())) {
             signColumn = StringUtils.substringBetween(createDML, COLLAPSING_MERGE_TREE_SIGN_PREFIX, ")");
         } else {
             log.error("Error: Trying to retrieve sign from table that is not CollapsingMergeTree");
@@ -159,6 +166,7 @@ public class DBMetadata {
 
     /**
      * Function to extract the version column for ReplacingMergeTree
+     *
      * @param createDML
      * @return Sign column
      */
@@ -166,21 +174,30 @@ public class DBMetadata {
 
         String versionColumn = "ver";
 
-        if(createDML.contains(TABLE_ENGINE.REPLICATED_REPLACING_MERGE_TREE.getEngine())) {
+        if (createDML.contains(TABLE_ENGINE.REPLICATED_REPLACING_MERGE_TREE.getEngine())) {
             String parameters = StringUtils.substringBetween(createDML, REPLICATED_REPLACING_MERGE_TREE_VER_PREFIX, ")");
-            if(parameters != null) {
+            if (parameters != null) {
                 String[] parameterArray = parameters.split(",");
-                if(parameterArray != null && parameterArray.length == 3) {
+                if (parameterArray != null && parameterArray.length == 3) {
                     versionColumn = parameterArray[2].trim();
-                } else if(parameterArray != null && parameterArray.length == 4) {
+                } else if (parameterArray != null && parameterArray.length == 4) {
                     versionColumn = parameterArray[2].trim() + "," + parameterArray[3].trim();
                 }
             }
-        }
-        else if(createDML.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.getEngine())) {
-            if(createDML != null && createDML.indexOf("(") != -1 && createDML.indexOf(")") != -1) {
+        } else if (createDML.contains(TABLE_ENGINE.SHARED_REPLACING_MERGE_TREE.getEngine())) {
+            String parameters = StringUtils.substringBetween(createDML, SHARED_REPLACING_MERGE_TREE_VER_PREFIX, ")");
+            if (parameters != null) {
+                String[] parameterArray = parameters.split(",");
+                if (parameterArray != null && parameterArray.length == 3) {
+                    versionColumn = parameterArray[2].trim();
+                } else if (parameterArray != null && parameterArray.length == 4) {
+                    versionColumn = parameterArray[2].trim() + "," + parameterArray[3].trim();
+                }
+            }
+        } else if (createDML.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.getEngine())) {
+            if (createDML != null && createDML.indexOf("(") != -1 && createDML.indexOf(")") != -1) {
                 String subString = StringUtils.substringBetween(createDML, REPLACING_MERGE_TREE_VER_PREFIX, ")");
-                if(subString != null) {
+                if (subString != null) {
                     versionColumn = subString.trim();
                 }
             }
@@ -190,14 +207,17 @@ public class DBMetadata {
 
         return versionColumn;
     }
+
     /**
      * Function to get table engine using system tables.
-     * @param conn ClickHouse Connection
+     *
+     * @param conn      ClickHouse Connection
      * @param tableName Table Name.
      * @return TABLE_ENGINE type
      */
-    public MutablePair<TABLE_ENGINE, String> getTableEngineUsingSystemTables(final ClickHouseConnection conn, final String database,
-                                                        final String tableName) {
+    public MutablePair<TABLE_ENGINE, String> getTableEngineUsingSystemTables(final ClickHouseConnection conn,
+                                                                             final String database,
+                                                                             final String tableName) {
         MutablePair<TABLE_ENGINE, String> result = new MutablePair<>();
 
 
@@ -206,12 +226,12 @@ public class DBMetadata {
                 log.error("Error with DB connection");
                 return result;
             }
-            try(Statement stmt = conn.createStatement()) {
+            try (Statement stmt = conn.createStatement()) {
                 String showSchemaQuery = String.format("select engine_full from system.tables where name='%s' and database='%s'",
                         tableName, database);
                 ResultSet rs = stmt.executeQuery(showSchemaQuery);
-                if(rs.wasNull() == false && rs.next()) {
-                    String response =  rs.getString(1);
+                if (!rs.wasNull() && rs.next()) {
+                    String response = rs.getString(1);
                     result = getEngineFromResponse(response);
                 } else {
                     log.debug("Error: Table not found in system tables:" + tableName + " Database:" + database);
@@ -220,7 +240,7 @@ public class DBMetadata {
                 stmt.close();
                 log.info("getTableEngineUsingSystemTables ResultSet" + rs);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             log.debug("getTableEngineUsingSystemTables exception", e);
         }
 
@@ -230,20 +250,18 @@ public class DBMetadata {
     public MutablePair<TABLE_ENGINE, String> getEngineFromResponse(String response) {
         MutablePair<TABLE_ENGINE, String> result = new MutablePair<>();
 
-        if(response.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.engine)) {
+        if (response.contains(TABLE_ENGINE.COLLAPSING_MERGE_TREE.engine)) {
             result.left = TABLE_ENGINE.COLLAPSING_MERGE_TREE;
             result.right = getSignColumnForCollapsingMergeTree(response);
-        }
-        else if(response.contains(TABLE_ENGINE.REPLICATED_REPLACING_MERGE_TREE.engine)) {
+        } else if (response.contains(TABLE_ENGINE.REPLICATED_REPLACING_MERGE_TREE.engine)) {
             result.left = TABLE_ENGINE.REPLICATED_REPLACING_MERGE_TREE;
             result.right = getVersionColumnForReplacingMergeTree(response);
-        }
-        else if(response.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.engine)) {
+        } else if (response.contains(TABLE_ENGINE.REPLACING_MERGE_TREE.engine)) {
             result.left = TABLE_ENGINE.REPLACING_MERGE_TREE;
             result.right = getVersionColumnForReplacingMergeTree(response);
-        } else if(response.contains(TABLE_ENGINE.MERGE_TREE.engine)) {
+        } else if (response.contains(TABLE_ENGINE.MERGE_TREE.engine)) {
             result.left = TABLE_ENGINE.MERGE_TREE;
-        }  else {
+        } else {
             result.left = TABLE_ENGINE.DEFAULT;
         }
 
@@ -254,6 +272,7 @@ public class DBMetadata {
     /**
      * Function to check if Replacing mergetree is supported
      * based on ClickHouse version.
+     *
      * @return true, if RMT is supported, false otherwise
      * @throws SQLException
      */
@@ -270,8 +289,6 @@ public class DBMetadata {
 
         return result;
     }
-
-
 
 
     /**
@@ -299,13 +316,13 @@ public class DBMetadata {
                 String columnDefinition = columns.getString("COLUMN_DEF");
                 String sqlDataType = columns.getString("SQL_DATA_TYPE");
                 String dataType = columns.getString("DATA_TYPE");
-               // String typeName = columns.getString("TYPE_NAME");
+                // String typeName = columns.getString("TYPE_NAME");
 //                String columnSize = columns.getString("COLUMN_SIZE");
 //                String isNullable = columns.getString("IS_NULLABLE");
 //                String isAutoIncrement = columns.getString("IS_AUTOINCREMENT");
 
                 // Skip generated columns.
-                if(isGeneratedColumn != null && isGeneratedColumn.equalsIgnoreCase("YES")) {
+                if (isGeneratedColumn != null && isGeneratedColumn.equalsIgnoreCase("YES")) {
                     continue;
                 }
                 result.put(columnName, typeName);
@@ -315,14 +332,15 @@ public class DBMetadata {
         }
         return result;
     }
+
     /**
      * Function to get the ClickHouse server timezone(Defaults to UTC)
      */
     public ZoneId getServerTimeZone(ClickHouseConnection conn) {
         ZoneId result = ZoneId.of("UTC");
-        if(conn != null) {
+        if (conn != null) {
             TimeZone serverTimeZone = conn.getServerTimeZone();
-            if(serverTimeZone != null) {
+            if (serverTimeZone != null) {
                 result = serverTimeZone.toZoneId();
             }
         }
