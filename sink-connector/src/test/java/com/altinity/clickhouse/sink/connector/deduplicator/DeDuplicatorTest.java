709858d138f28b8b4aaf0cfedebb3d292bf50e0f
Viktor Poluksht <vpoluksht@gmail.com>
2026-06-22 09:45:26 +0200

    fix: bound de-duplicator memory use

--- sink-connector/src/test/java/com/altinity/clickhouse/sink/connector/deduplicator/DeDuplicatorTest.java
+++ sink-connector/src/test/java/com/altinity/clickhouse/sink/connector/deduplicator/DeDuplicatorTest.java
@@ -50,11 +50,40 @@ public class DeDuplicatorTest {
         SinkRecord recordDifferentKey = ClickHouseSinkTaskTest.spoofSinkRecord(topic, keyField, "22", valueField, value,
                 TimestampType.NO_TIMESTAMP_TYPE, timestamp2);
         boolean resultDifferentKey = dedupe.isNew(topic, recordDifferentKey);
-        Assert.assertTrue(resultDifferentKey == false);
+        Assert.assertTrue(resultDifferentKey == true);
 
         // Test Different Topic.
         boolean resultDifferentTopic = dedupe.isNew("employees", recordTwo);
         Assert.assertTrue(resultDifferentTopic == true);
 
+    }
+
+    @Test
+    public void testDedupePoolEvictsOldKeys() {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(ClickHouseSinkConnectorConfigVariables.DEDUPLICATION_POLICY.toString(), "old");
+        properties.put(ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT.toString(), "2");
+
+        ClickHouseSinkConnectorConfig config = new ClickHouseSinkConnectorConfig(properties);
+        DeDuplicator dedupe = new DeDuplicator(config);
+
+        String topic = "products";
+        String keyField = "productId";
+        String valueField = "amount";
+        String value = "2000";
+
+        SinkRecord recordOne = ClickHouseSinkTaskTest.spoofSinkRecord(topic, keyField, "11", valueField, value,
+                TimestampType.NO_TIMESTAMP_TYPE, System.currentTimeMillis());
+        SinkRecord recordTwo = ClickHouseSinkTaskTest.spoofSinkRecord(topic, keyField, "22", valueField, value,
+                TimestampType.NO_TIMESTAMP_TYPE, System.currentTimeMillis());
+        SinkRecord recordThree = ClickHouseSinkTaskTest.spoofSinkRecord(topic, keyField, "33", valueField, value,
+                TimestampType.NO_TIMESTAMP_TYPE, System.currentTimeMillis());
+        SinkRecord recordOneAgain = ClickHouseSinkTaskTest.spoofSinkRecord(topic, keyField, "11", valueField, value,
+                TimestampType.NO_TIMESTAMP_TYPE, System.currentTimeMillis());
+
+        Assert.assertTrue(dedupe.isNew(topic, recordOne));
+        Assert.assertTrue(dedupe.isNew(topic, recordTwo));
+        Assert.assertTrue(dedupe.isNew(topic, recordThree));
+        Assert.assertTrue(dedupe.isNew(topic, recordOneAgain));
     }
 }
